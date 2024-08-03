b484d979a9cecdeda550fe6f15c981cd3131b862
Diego Teixeira de Almeida <diegotx2008@gmail.com>
2024-08-03 11:11:45 -0400

    Subindo estrutura do projeto

---
+++ DTA Financial Hub/dtafinancialub/src/main/java/br/com/dtafinancialub/DtafinancialubApplication.java
@@ -0,0 +1,13 @@
+package br.com.dtafinancialub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtafinancialubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtafinancialubApplication.class, args);
+	}
+
+}
